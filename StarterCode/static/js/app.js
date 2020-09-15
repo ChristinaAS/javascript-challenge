2cf330854a96d24f786dcff92696ad026fb6ce15
ChristinaAS <smiley9824@aol.com>
2020-09-14 22:04:04 -0400

    changes

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,41 +9,43 @@ var tbody = d3.select("tbody")
 console.log(tableData);
 
 function displayTable(tableData) {
-    tableData.forEach(function(ufoReport) {
-    //console.log(ufoReport);
+    tableData.forEach(function (ufoReport) {
+        //console.log(ufoReport);
         var row = tbody.append("tr");
-        Object.entries(ufoReport).forEach(function([key, value]) {
-        //console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
+        Object.entries(ufoReport).forEach(function ([key, value]) {
+            //console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
 }
-var button = d3.select("filter-btn")
+var button = d3.select("#filter-btn")
 var dateTime = d3.select("#datetime")
+//var filters = {}
 
 
-//button.on("click", function(){
-  //  var value = ; 
-//{
-    //d3.event.preventDefault();
-    //deleteTbo})
-function sortData(tableData) {
+button.on("click", function (sortData) {
     d3.event.preventDefault();
-    //var DateTime = d3.select("#datetime").property("value");
-    var filteredDateTime = tableData.filter(function(d) {
-        return d.datetime === $('#datetime').val();
-    });
-    return filteredDateTime;
-    //var filteredData = tableData.filter(ufoReport => ufoReport.datetime === dateTime.trim)
-//ufoReport(filteredData);
-};
+    var date = d3.select("#datetime").property("value");
+    //console.log(date)
+    var filteredData = tableData.filter(ufoFiltered => ufoFiltered.datetime === dateTime.property("value"));
+    displayTable(filteredData)
+});
+//sortData(tableData)
 
-displayTable(data);
-button.on("click", function() {
-    var result = sortData(data);
-    displayTable(result);
-})
+//function updateFilters() {
+ //   var filterData = tableData
+ //   Object.entries(filters).forEach(function ([key, value]) {
+//        filterData = filterData.filter(row => row[key] === value);
+//    });
+ //   displayTable(filterData);
+//}
 
-d3.selectAll("#filter-btn").on("click", sortData);
-displayTable(tableData);
+//displayTable(tableData);
+//button.on("click", function() {
+   // var result = sortData(tableData);
+    //displayTable(result);
+//})
+
+//d3.selectAll("#filter-btn").on("click", sortData);
+//displayTable(filteredData);
